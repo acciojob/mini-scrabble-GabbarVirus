2ecc9f3cdb84d32d8fe4fce44461a8f37fa3a860
Rohit Rai <66778858+GabbarVirus@users.noreply.github.com>
2023-01-16 16:37:01 +0530

    scrabble

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+document.getElementById("evaluatedText").value=""; // initially string is empty so count is also empty
+document.addEventListener("keyup", function ()
+{
+    count=document.getElementById("evaluatedText").value; // here thisvalue will return the string
+    document.querySelector("#letterCount").textContent=count.length;
+})
